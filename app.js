af312d35809ef12289e5c03d674b94bfa8c40f82
Pedro Castro <contatoaugustopedro@gmail.com>
2023-12-06 17:34:32 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-const questions = document.querySelectorAll(".question");
+const questions = [
+  {
+    id: 1,
+    quest: "Vocês aceitam a maioria das bandeiras de cartões?",
+    info: "Sim, aceitamos todas as bandeiras de cartões.",
+  },
+  {
+    id: 2,
+    quest: "Vocês suportam fazendeiros locais?",
+    info: "Sim, todos os produtos são comprados de fazendeiros locais.",
+  },
+  {
+    id: 3,
+    quest: "Vocês usam ingredientes orgânicos?",
+    info: "Sim, todos os produtos são de origem orgânica.",
+  },
+];
 
-questions.forEach((question) => {
-  const btn = question.querySelector(".question-btn");
-  btn.addEventListener("click", () => {
-    question.classList.toggle("show-text");
-  });
+
+window.addEventListener("DOMContentLoaded", () => {
+  displayQuestionsItens(questions);
 });
+
+const faqContainer = document.querySelector(".faq-container");
+
+function displayQuestionsItens(questions) {
+  const displayQuestions = questions
+    .map((item) => {
+      return `<article class="question">
+                <div class="question-header">
+                  <p>${item.quest}</p>
+                  <button class="btn question-btn">
+                    <span class="plusBtn"><i class="fa-solid fa-plus"></i></span>
+                    <span class="minusBtn"><i class="fa-solid fa-minus"></i></span>
+                  </button>
+                </div>
+                <p class="description">${item.info}</p>
+              </article>`;
+    })
+    .join(" ");
+
+  faqContainer.innerHTML += displayQuestions;
+
+  const questionsItens = document.querySelectorAll(".question");
+
+  questionsItens.forEach((question) => {
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", () => {
+      question.classList.toggle("show-text");
+    });
+  });
+}
--- menu.js
+++ menu.js
@@ -1,83 +1,83 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
-    price: 15.99,
-    img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    title: "cafezinho",
+    category: "café",
+    price: "4,00",
+    img: "./images/pao.jpg",
+    desc: `Pão quentinho para começar o dia bem com um café`,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "lunch",
-    price: 13.99,
-    img: "./images/item-2.jpeg",
-    desc: `Vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    title: "strogonoff",
+    category: "almoço",
+    price: "12,00",
+    img: "./images/strogonoff.jpg",
+    desc: `Strogonoff de frango, uma delicia hummmm`,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 6.99,
-    img: "./images/item-3.jpeg",
-    desc: `Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    title: "pudim",
+    category: "sobremesa",
+    price: "8,00",
+    img: "./images/pudim.jpg",
+    desc: `Melhor pudim da cidade, esse sim foi feito para você.`,
   },
   {
     id: 4,
-    title: "country delight",
-    category: "breakfast",
-    price: 20.99,
-    img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    title: "pão de queijo",
+    category: "café",
+    price: "6,00",
+    img: "./images/pao-de-queijo.jpg",
+    desc: `Pão de queijo bem fofinho.`,
   },
   {
     id: 5,
-    title: "egg attack",
-    category: "lunch",
-    price: 22.99,
-    img: "./images/item-5.jpeg",
-    desc: `Franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    title: "Churrasco",
+    category: "almoço",
+    price: "20,00",
+    img: "./images/churrasco.jpg",
+    desc: `Churrasco no ponto ideal para matar sua fome.`,
   },
   {
     id: 6,
-    title: "oreo dream",
-    category: "shakes",
-    price: 18.99,
-    img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    title: "Milkshake",
+    category: "sobremesa",
+    price: "9,00",
+    img: "./images/milkshake.jpg",
+    desc: `Melhor milkshake da cidade, leite recém tirado da vaca.`,
   },
   {
     id: 7,
-    title: "bacon overflow",
-    category: "breakfast",
-    price: 8.99,
-    img: "./images/item-7.jpeg",
-    desc: `Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    title: "esfiha",
+    category: "café",
+    price: "6,00",
+    img: "./images/esfiha.jpg",
+    desc: `Esfiha de frango para começar o dia bem meu amigão.`,
   },
   {
     id: 8,
-    title: "american classic",
-    category: "lunch",
-    price: 12.99,
-    img: "./images/item-8.jpeg",
-    desc: `On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    title: "feijoada",
+    category: "almoço",
+    price: "15,00",
+    img: "./images/feijoada.jpg",
+    desc: `Melhor feijoada do Brasil, talvez até da cidade.`,
   },
   {
     id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
-    price: 16.99,
-    img: "./images/item-9.jpeg",
-    desc: `Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    title: "sorvete",
+    category: "sobremesa",
+    price: "7,00",
+    img: "./images/sorvete.jpg",
+    desc: `Sorvete bem geladinho para alegrar seu dia.`,
   },
   {
     id: 10,
-    title: "bison steak",
-    category: "dinner",
-    price: 22.99,
-    img: "./images/item-10.jpeg",
-    desc: `Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    title: "parmegiana",
+    category: "jantar",
+    price: "20,00",
+    img: "./images/parmegiana.jpg",
+    desc: `Melhor parmegiana, minha mãe que passou a receita.`,
   },
 ];
 
@@ -86,16 +86,28 @@ const btnsContainer = document.querySelector(".btns-container");
 
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItens(menu);
-  const categories = menu.reduce(
-    function (values, item) {
-      if (!values.includes(item.category)) {
-        values.push(item.category);
-      }
-      return values;
-    },
-    ["all"]
-  );
+
+  const categories = ["all"];
+
+  menu.forEach((item) => {
+    if (!categories.includes(item.category)) {
+      categories.push(item.category);
+    }
+  });
+
+  // const categories = menu.reduce(
+  //   function (values, item) {
+  //     if (!values.includes(item.category)) {
+  //       values.push(item.category);
+  //     }
+  //     return values;
+  //   },
+  //   ["all"]
+  // );
+
   displayMenuBtns(categories);
+
+  console.log(categories);
 });
 
 function displayMenuBtns(categories) {
@@ -130,16 +142,16 @@ function displayMenuBtns(categories) {
 function displayMenuItens(menuItens) {
   let displayMenu = menuItens
     .map(function (item) {
-      return `<article class="menu-item">
+      return `<div class="menu-item">
                 <img src="${item.img}" alt="pancake" class="photo" />
-                  <div class="item-infos">
-                    <header>
-                      <h4>${item.title}</h4>
-                      <h4 class="price">${item.price}</h4>
-                    </header>
-                    <p>${item.desc}</p>
-                  </div>
-              </article>`;
+                <div class="item-infos">
+                  <header>
+                    <h4>${item.title}</h4>
+                    <h4 class="price">${item.price}</h4>
+                  </header>
+                  <p>${item.desc}</p>
+                </div>
+              </div>`;
     })
     .join(" ");
   sectionCenter.innerHTML = displayMenu;
